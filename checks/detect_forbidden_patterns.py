39344f988bd807d751dc516f7b4919c7c90a1495
Developer <developer@local>
2025-12-05 21:59:36 +0900

    chore: add legacy checks package shims to satisfy mypy

---
+++ checks/detect_forbidden_patterns.py
@@ -0,0 +1,6 @@
+"""Legacy import shim for `checks.detect_forbidden_patterns`.
+
+Delegates to `scripts.checks.detect_forbidden_patterns`.
+"""
+
+from scripts.checks.detect_forbidden_patterns import *  # noqa: F401,F403
---
+++ checks/validate_session.py
@@ -0,0 +1,6 @@
+"""Legacy import shim for `checks.validate_session`.
+
+Delegates to `scripts.checks.validate_session`.
+"""
+
+from scripts.checks.validate_session import *  # noqa: F401,F403
